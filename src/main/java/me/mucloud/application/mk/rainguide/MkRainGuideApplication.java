3e9f746e213c1b3aafd67736a5b581b65cb6bc5d
Mu_Cloud <Mu_Cloud@outlook.de>
2026-03-01 17:06:03 +0800

    Init Structure.

---
+++ src/main/java/me/mucloud/application/mk/rainguide/MkRainGuideApplication.java
@@ -0,0 +1,13 @@
+package me.mucloud.application.mk.rainguide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MkRainGuideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MkRainGuideApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/mucloud/application/mk/rainguide/MkRainGuideApplicationTests.java
@@ -0,0 +1,13 @@
+package me.mucloud.application.mk.rainguide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MkRainGuideApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
